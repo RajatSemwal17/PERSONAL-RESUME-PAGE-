4336278f403ce963a58d4b925179f1f5b9ece19b
RishabhXD357 <rishabh0690.becse24@chitkara.edu.in>
2025-04-23 14:24:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ if (currMode === "dark") {
     body.classList.add("light");
     moon.querySelector("i").classList.replace("fa-sun", "fa-moon");
 }
+// ☀️ Toggle on click
+moon.addEventListener("click", () => {
+    if (currMode === "light") {
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+        moon.querySelector("i").classList.replace("fa-moon", "fa-sun");
+    } else {
+        currMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+        moon.querySelector("i").classList.replace("fa-sun", "fa-moon");
+    }
