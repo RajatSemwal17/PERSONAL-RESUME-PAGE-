4bcddd0c6f48d04dd8926ce52f98a458b6ad76ad
SyntaxBugger <tuhin4174@gmail.com>
2025-04-24 19:26:28 +0530

    Update wex.js
    added button in js

--- wex.js
+++ wex.js
@@ -2,3 +2,5 @@ document.addEventListener("DOMContentLoaded", function () {
     const expandButtons = document.querySelectorAll(".expand-btn");
     expandButtons.forEach(button => {
         button.addEventListener("click", function () {
+            const moreInfo = this.nextElementSibling;
+            moreInfo.classList.toggle("show");
